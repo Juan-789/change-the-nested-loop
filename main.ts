974950d23bae655ae0722f6d55242e182077a381
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-04-08 12:34:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let y = 0
+let x = 0
+input.onButtonPressed(Button.A, function () {
+    while (y <= 0) {
+        while (x <= 0) {
+            for (let index = 0; index < 5; index++) {
+                led.plot(x, y)
+                basic.pause(100)
+                x += 1
+            }
+        }
+        y += 1
+        x = 0
+    }
+    basic.pause(100)
+})
 input.onGesture(Gesture.Shake, function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= 4; y++) {
